6ab2f174d2922216140aa8742a0ad011dbeb9e71
Matvei-Lise <89886662592@mail.ru>
2022-11-01 17:27:57 +0300

    PZ_4 was done

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,3 +1,18 @@
-#
-x = float(input())
-n = int(input())
+# Дано вещественное число Х и целое число N(>0). Найти значение выражения 1+Х+Х^2/(2!)+...+Х^N/(N!).
+try:
+    x = float(input())
+    n = int(input())
+    sum_ = 0
+    i = 0
+    while i <= n:
+        s = 1
+        k = i
+        while k:
+            s *= k
+            k -= 1
+        sum_ += x ** i / s
+        i = i + 1
+    print(sum_)
+except ValueError:
+    print('Ошибка')
+
