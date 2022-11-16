fac5437f6cf3b55bdaa7c20f999e83b67b669e89
Kubik007 <kubaramuradova1764@gmail.com>
2022-11-16 20:53:50 +0300

    PZ_6_3

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,15 +1,7 @@
 # Дан список размера N, все элементы которого, кроме первого, упорядочены по
 # возрастанию. Сделать список упорядоченным, переместив первый элемент на новую
 # позицию.
-data_list = [6, 1, 2, 3, 4, 5, 7, 8, 9]
-print(data_list)
-elem = data_list.pop(0)
-nev = []
-i = 1
-for i, x in enumerate(data_list):  # получаем индек и значение элементов списка
-    if data_list[i] > elem:
-        nev.append(elem)
-        break
-    nev.append(x)
-nev = nev + data_list[i::]
-print(nev)
+my_list = [3, 1, 2, 4, 5]
+print(my_list)
+my_list.sort()
+print(my_list)
