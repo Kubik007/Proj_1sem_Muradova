94798eb8fba75362f14d36ce6bf37051e5174dc3
Kubik007 <kubaramuradova1764@gmail.com>
2023-01-31 22:17:18 +0300

    small commit

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,7 +1,15 @@
 # В последовательности на n целых элементов найти произведение элементов средней трети.
 from random import randint
 
-n = int(input("Введите количество целых элементов: "))
+n = 3 * randint(2, 3)
 list_1 = []
 for i in range(int(n)):
     list_1.append(randint(0, 10))
+print("Целые числа: ", list_1)
+
+list_2 = [i for i in list_1[n // 3:n // 3 * 2]]
+new_list = 1
+for i in range (len(list_2)):
+    new_list *= list_2[i]
+
+print("Произведение элементов средней трети = ", new_list)
